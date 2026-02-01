1145fdfb2765a53bb45ba2b929723e942f044b42
Henrique Moreira <henrique@declaratived.com>
2026-02-01 13:20:06 +0000

    Basic decimal formatting '0.00'

--- src/packages/conta/xcreator.py
+++ src/packages/conta/xcreator.py
@@ -9,6 +9,9 @@ from .wcell import GenCell
 
 
 class ExcelGera(GenCell):
+    """ Excel generator. """
+    numeric_fmt = "0.00"
+
     def __init__(self, data, outname="out.xlsx", name=""):
         super().__init__(data=data if data else [], name=name if name else outname)
         self.outname = outname
@@ -22,6 +25,13 @@ class ExcelGera(GenCell):
         self._adjust_widths(ws)
         wbk.save(self.outname)
 
+    def _best_cell_form(self, cell):
+        """ Format cell basically """
+        if cell.value is None:
+            return None
+        cell.number_format = "0.00"
+        return cell
+
     def _adjust_widths(self, ws):
         """ Larguras de colunas. """
         pigment = 4
@@ -32,4 +42,7 @@ class ExcelGera(GenCell):
                 val = str(cell.value) if cell.value is not None else ""
                 if len(val) > max_len:
                     max_len = len(val)
+                if cell.value is not None and isinstance(cell.value, (float, int)):
+                    if isinstance(ExcelGera.numeric_fmt, str):
+                        cell.number_format = ExcelGera.numeric_fmt
             ws.column_dimensions[col_letter].width = max_len + pigment
